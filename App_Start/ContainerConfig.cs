4eed1a89eac1037fc33ae453833d82ebf16f1483
Jesudunsin-Babatunde <jesudunsinbabatunde@gmail.com>
2020-02-27 20:39:26 +0100

    initial commit

---
+++ App_Start/ContainerConfig.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using BankData.DI;
+using BankData.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MicroFinBank.App_Start
+{
+    public class ContainerConfig
+    {
+        public static void RegisterAutofac()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<Function>().As<IFunction>().InstancePerRequest();
+            builder.RegisterType<MicroFinDbEntities2>().InstancePerRequest();
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
